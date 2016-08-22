301855e7919d212f25d33213fa24ab88df77e144
cobus <cobus@cobus-VirtualBox>
2016-08-22 23:29:47 +0900

    data structure. * tree * expression tree

--- DataStructure/src/DataStructure.c
+++ DataStructure/src/DataStructure.c
@@ -20,6 +20,8 @@
 #include "stack/MazeArrayStack.h"
 #include "queue/MazeQueue.h"
 
+#include "tree/BinaryTree.h"
+#include "tree/ExpressionTree.h"
 
 void ArrayStackTest() {
 	printf("----- array stack\n");
@@ -143,6 +145,73 @@ void dllDequeTest() {
 
 }
 
+void printBTData(BTData data) {
+	printf("%d ", data);
+}
+
+void basicTreeTest() {
+	BinaryTreeNode *bt1 = makeBinaryTreeNode();
+	BinaryTreeNode *bt2 = makeBinaryTreeNode();
+	BinaryTreeNode *bt3 = makeBinaryTreeNode();
+	BinaryTreeNode *bt4 = makeBinaryTreeNode();
+	BinaryTreeNode *bt5 = makeBinaryTreeNode();
+	BinaryTreeNode *bt6 = makeBinaryTreeNode();
+
+	setBTData(bt1, 1);
+	setBTData(bt2, 2);
+	setBTData(bt3, 3);
+	setBTData(bt4, 4);
+	setBTData(bt5, 5);
+	setBTData(bt6, 6);
+
+	makeLeftSubTree(bt1, bt2);
+	makeRightSubTree(bt1, bt3);
+	makeLeftSubTree(bt2, bt4);
+	makeRightSubTree(bt2, bt5);
+	makeLeftSubTree(bt3, bt6);
+
+	/**
+	 * 현재 구조.
+	 *         1
+	 *      2      3
+	 *    4   5  6
+	 */
+	printf("\n---------------- Binary Tree\n");
+	printf("%d\n", getBTData(getLeftSubTree(bt1)));
+	printf("%d\n", getBTData(getLeftSubTree(getLeftSubTree(bt1))));
+
+	printf("\n---------------- inOrder Binary Tree\n");
+	inOrderBinaryTreeTraverse(bt1, printBTData); // 4 2 5 1 6 3
+
+	printf("\n---------------- preOrder Binary Tree\n");
+	preOrderBinaryTreeTraverse(bt1, printBTData); // 1 2 4 5 3 6
+
+
+	printf("\n---------------- postOrder Binary Tree\n");
+	postOrderBinaryTreeTraverse(bt1, printBTData); // 4 5 2 6 3 1
+}
+
+void expressionTreeTest() {
+	char *exp = "12+7*";
+	BinaryTreeNode *tree = makeExpTree(exp);
+
+	printf("\n");
+
+	printf("preOrder: ");
+	showPrefixTypeExp(tree);
+	printf("\n"); // preOrder: * + 1 2 7
+
+	printf("inOrder: ");
+	showInfixTypeExp(tree);
+	printf("\n"); // inOrder: 1 + 2 * 7
+
+	printf("postOrder: ");
+	showPostfixTypeExp(tree);
+	printf("\n"); // postOrder: 1 2 + 7 *
+
+	printf("%s = %d\n", exp, evaluateExpTree(tree)); // 12+7* = 21
+}
+
 int main(void) {
 
 	ArrayStackTest();
@@ -155,6 +224,9 @@ int main(void) {
 
 	solveMazeByStack();
 	solveMazeByQueue();
+
+	basicTreeTest();
+	expressionTreeTest();
 
 
 	return EXIT_SUCCESS;
--- DataStructure/src/tree/BinaryTree.h
+++ DataStructure/src/tree/BinaryTree.h
@@ -26,4 +26,25 @@ BinaryTreeNode *getRightSubTree(BinaryTreeNode *tree);
 void makeLeftSubTree(BinaryTreeNode *mainTree, BinaryTreeNode *subTree);
 void makeRightSubTree(BinaryTreeNode *mainTree, BinaryTreeNode *subTree);
 
+void deleteBinaryTree(BinaryTreeNode *tree);
+
+/**
+ * node 순회 시, 실행할 함수 정의.
+ */
+typedef void visitActionFuncPtr(BTData data);
+
+/*
+ * 중위 순회.
+ */
+void inOrderBinaryTreeTraverse(BinaryTreeNode *tree, visitActionFuncPtr action);
+/**
+ * 전위 순회.
+ */
+void preOrderBinaryTreeTraverse(BinaryTreeNode *tree, visitActionFuncPtr action);
+/**
+ * 후위 순회.
+ */
+void postOrderBinaryTreeTraverse(BinaryTreeNode *tree,
+		visitActionFuncPtr action);
+
 #endif /* TREE_BINARYTREE_H_ */
